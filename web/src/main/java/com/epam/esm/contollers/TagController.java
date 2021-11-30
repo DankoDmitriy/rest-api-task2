b3575ed5a7af5ef540c9458e888ebc9672293a10
Dmitriy Danko <danko.dima@gmail.com>
2021-11-30 13:17:03 +0300

    Added TagController

---
+++ web/src/main/java/com/epam/esm/contollers/TagController.java
@@ -0,0 +1,49 @@
+package com.epam.esm.contollers;
+
+import com.epam.esm.TagService;
+import com.epam.esm.impl.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/tags", produces = MediaType.APPLICATION_JSON_VALUE)
+public class TagController {
+    private TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @GetMapping
+    public List<Tag> getAllTags() {
+        List<Tag> tagItems = tagService.findAll();
+        return tagItems;
+    }
+
+    @GetMapping("/{id}")
+    public Tag getTagById(@PathVariable("id") long id) {
+        return tagService.findById(id);
+    }
+
+    @PostMapping
+    public Tag addTag(@RequestBody Tag tag) {
+        tagService.save(tag);
+        return tag;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTagById(@PathVariable("id") long id) {
+        tagService.delete(id);
+        return "delete";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateTagById(@RequestBody Tag tag, @PathVariable("id") long id) {
+        tagService.update(id, tag);
+        return "update";
+    }
+}
