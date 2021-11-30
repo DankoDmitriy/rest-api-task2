1950e58ec6569242b5f200e14baea2d024a2635a
Dmitriy Danko <danko.dima@gmail.com>
2021-11-30 13:05:31 +0300

    Added TagService interface and TagServiceImpl class

---
+++ service/src/main/java/com/epam/esm/impl/TagServiceImpl.java
@@ -0,0 +1,43 @@
+package com.epam.esm.impl;
+
+import com.epam.esm.TagDao;
+import com.epam.esm.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class TagServiceImpl implements TagService {
+    private TagDao tagDao;
+
+    @Autowired
+    public TagServiceImpl(TagDao tagDao) {
+        this.tagDao = tagDao;
+    }
+
+    @Override
+    public List<Tag> findAll() {
+        return tagDao.findAll();
+    }
+
+    @Override
+    public Tag findById(Long id) {
+        return tagDao.findById(id);
+    }
+
+    @Override
+    public void save(Tag tag) {
+        tagDao.save(tag);
+    }
+
+    @Override
+    public void delete(Long id) {
+        tagDao.delete(id);
+    }
+
+    @Override
+    public void update(Long id, Tag tag) {
+        tagDao.update(id, tag);
+    }
+}
