e94843fa61990e452f435012ec2b3090562b0829
Dmitriy Danko <danko.dima@gmail.com>
2021-12-03 10:46:48 +0300

    Added custom RowMapper for GiftCertificate.

--- persistence/src/main/java/com/epam/esm/imp/GiftCertificateDaoImpl.java
+++ persistence/src/main/java/com/epam/esm/imp/GiftCertificateDaoImpl.java
@@ -1,9 +1,9 @@
 package com.epam.esm.imp;
 
 import com.epam.esm.GiftCertificateDao;
+import com.epam.esm.GiftCertificateRowMapper;
 import com.epam.esm.impl.GiftCertificate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
@@ -17,8 +17,8 @@ import java.util.Optional;
 
 @Component
 public class GiftCertificateDaoImpl implements GiftCertificateDao {
-    private static final String SQL_FIND_ALL_GIFT_CERTIFICATES = "SELECT * FROM gift_certificate";
-    private static final String SQL_FIND_BY_GIFT_CERTIFICATE = "SELECT * From gift_certificate WHERE id=?";
+    private static final String SQL_FIND_ALL_GIFT_CERTIFICATES = "SELECT gift.id as gift_id, gift.name as gift_name, gift.description, gift.price, gift.duration, gift.create_date, gift.last_update_date, tag.id as tag_id, tag.name as tag_name FROM gift_certificate as gift LEFT JOIN certificate_tag as link  ON link.gift_sercificate_id = gift.id LEFT JOIN tag ON link.tag_id=tag.id ORDER BY gift.id";
+    private static final String SQL_FIND_BY_ID_GIFT_CERTIFICATE = "SELECT gift.id as gift_id, gift.name as gift_name, gift.description, gift.price, gift.duration, gift.create_date, gift.last_update_date, tag.id as tag_id, tag.name as tag_name FROM gift_certificate as gift LEFT JOIN certificate_tag as link  ON link.gift_sercificate_id = gift.id LEFT JOIN tag ON link.tag_id=tag.id where gift.id=?";
     private static final String SQL_SAVE_GIFT_CERTIFICATE = "INSERT INTO gift_certificate (name, description, price, duration, create_date, last_update_date) VALUES(?,?,?,?,?,?)";
     private static final String SQL_DELETE_GIFT_CERTIFICATE = "DELETE FROM gift_certificate WHERE id=?";
     private static final String SQL_UPDATE_GIFT_CERTIFICATE = "UPDATE gift_certificate SET name=?, description=?, price=?, duration=?, create_date=?, last_update_date=? WHERE id=?";
@@ -32,12 +32,12 @@ public class GiftCertificateDaoImpl implements GiftCertificateDao {
 
     @Override
     public List<GiftCertificate> findAll() {
-        return jdbcTemplate.query(SQL_FIND_ALL_GIFT_CERTIFICATES, new BeanPropertyRowMapper<>(GiftCertificate.class));
+        return jdbcTemplate.query(SQL_FIND_ALL_GIFT_CERTIFICATES, new GiftCertificateRowMapper());
     }
 
     @Override
     public Optional<GiftCertificate> findById(Long id) {
-        return jdbcTemplate.query(SQL_FIND_BY_GIFT_CERTIFICATE, new BeanPropertyRowMapper<>(GiftCertificate.class), id)
+        return jdbcTemplate.query(SQL_FIND_BY_ID_GIFT_CERTIFICATE, new GiftCertificateRowMapper(), id)
                 .stream().findAny();
     }
 
