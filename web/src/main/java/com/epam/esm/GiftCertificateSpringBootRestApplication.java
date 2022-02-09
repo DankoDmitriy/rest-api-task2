14748ee55fc1986b868aecf3af10c7e602d97938
Dmitriy Danko <danko.dima@gmail.com>
2022-02-09 14:41:30 +0300

    add servletInit

--- web/src/main/java/com/epam/esm/GiftCertificateSpringBootRestApplication.java
+++ web/src/main/java/com/epam/esm/GiftCertificateSpringBootRestApplication.java
@@ -2,17 +2,37 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @SpringBootApplication
-public class GiftCertificateSpringBootRestApplication implements WebMvcConfigurer {
+public class GiftCertificateSpringBootRestApplication extends SpringBootServletInitializer implements WebMvcConfigurer {
     
     private final static String LOCALIZATION_PATCH = "localization/error_messages";
 
     public static void main(String[] args) {
         SpringApplication.run(GiftCertificateSpringBootRestApplication.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GiftCertificateSpringBootRestApplication.class);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        PageableHandlerMethodArgumentResolver pageableResolver = new PageableHandlerMethodArgumentResolver();
+        pageableResolver.setFallbackPageable(PageRequest.of(1, 2));
+        resolvers.add(pageableResolver);
+        WebMvcConfigurer.super.addArgumentResolvers(resolvers);
     }
 
     @Bean
